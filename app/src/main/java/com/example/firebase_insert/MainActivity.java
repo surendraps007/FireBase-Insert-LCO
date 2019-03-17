2f43b7670626347021e14f27a16485045b89dfe5
Surendra Pratap Singh <surendrapratapsingh@Surendras-MacBook-Pro.local>
2019-03-17 15:41:26 +0530

    Fire-Insert

---
+++ app/src/main/java/com/example/firebase_insert/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.firebase_insert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+// ...
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.setValue("get it done");
+    }
+}
